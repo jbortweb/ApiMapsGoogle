fe7b7185e03bb5b099bac585247f9d30c4dc2b12
jbortweb <jbortweb@gmail.com>
2021-07-28 13:35:21 +0200

    Autocompletado

--- localizacion.js
+++ localizacion.js
@@ -6,6 +6,8 @@ class Localizacion{
             navigator.geolocation.getCurrentPosition((position)=>{
                 this.latitude = position.coords.latitude;
                 this.longitude = position.coords.longitude;
+
+                callback();
             });
         }else{
             alert('Tu navegador no soporta la geolocalizacion');
--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-fuction initMap(){
+google.maps.event.addDomListener(window, 'load', function(){
 
-    cons ubicacion = new Localizacion(()=>{
+    const ubicacion = new Localizacion(()=>{
 
-        cons options = {
-            center: {
+        const myLatLng = {lat: ubicacion.latitude, lng: ubicacion.longitude};
 
-                lat: ubicacion.latitude,
-                lng: ubicacion.longitude
-            },
+        var texto = '<h1> Nombre del lugar </h1>' + '<p> Descripción del lugar</p>' + '<a href="https://www.google.com">Página web</a>';
+
+        const options = {
+            center: myLatLng,
+            zoom: 15
         }
-    })
-}
+
+        var map = document.getElementById('map');
+
+        const mapa = new google.maps.Map(map, options);
+
+        const marcador = new google.maps.Marker({
+            position: myLatLng,
+            map: mapa,
+            title: 'Marcador'
+        });
+
+        var informacion = new google.maps.InfoWindow({
+            content: texto
+        });
+
+        marcador.addListener('click', function(){
+            informacion.open(mapa, marcador);
+        });
+
+        var autocomplete = document.getElementById('autocomplete');
+
+        const search = new google.maps.places.Autocomplete(autocomplete);
+        search.bindTo('bounds', mapa);
+    });
+});
